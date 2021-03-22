fc4196684ac0c19183d1616ee19c1303f475d667
vaishu <vaishupriya90@gmail.com>
2021-03-21 21:47:33 -0500

    initial commit

---
+++ src/test/java/com/vaishu/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaishu.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
