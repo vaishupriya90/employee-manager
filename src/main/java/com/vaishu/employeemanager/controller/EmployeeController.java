e34ec45d65cfae2347153a529a1468c1ff6cac24
vaishu <vaishupriya90@gmail.com>
2021-03-25 01:24:40 -0500

    no changes made to the code.just checked if the app runs fine and the controller works fine by using postman app, sending requests from postman

--- src/main/java/com/vaishu/employeemanager/controller/EmployeeController.java
+++ src/main/java/com/vaishu/employeemanager/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("employee")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
